fdd89494e24ff1cc67943134f883cb55ff05db0b
Opjasson <azmitheusos333@gmail.com>
2026-06-06 17:23:10 +0700

    optimalized : add update product menu

--- app/src/main/java/com/example/nasibakarjoss18_application/Activity/LaporanPenjualanActivity.kt
+++ app/src/main/java/com/example/nasibakarjoss18_application/Activity/LaporanPenjualanActivity.kt
@@ -165,7 +165,7 @@ class LaporanPenjualanActivity : AppCompatActivity() {
         userViewModel.userLogin.observe(this) { user ->
             if (user?.role == "kasir") {
                 navigationView.menu.findItem(R.id.menu_manageProduct).isVisible = false
-                navigationView.menu.findItem(R.id.menu_laporan).isVisible = false
+
                 
                 // Keamanan tambahan: Redirect kasir jika mencoba masuk ke halaman laporan
                 Toast.makeText(this, "Akses ditolak: Halaman khusus Admin", Toast.LENGTH_SHORT).show()
