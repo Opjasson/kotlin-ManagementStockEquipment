71d69762edc6aece184a56d28ce598cedf1daf5a
Opjasson <azmitheusos333@gmail.com>
2026-06-08 08:24:55 +0700

    optimalized : add verification stock amount if stock sold out

--- app/src/main/java/com/example/nasibakarjoss18_application/Adapter/CardProductListAdapter.kt
+++ app/src/main/java/com/example/nasibakarjoss18_application/Adapter/CardProductListAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.nasibakarjoss18_application.Adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.nasibakarjoss18_application.Domain.ProductModel
+import com.example.nasibakarjoss18_application.R
 import com.example.nasibakarjoss18_application.databinding.ViewHolderCartListBinding
 
 class CardProductListAdapter(
@@ -33,13 +37,25 @@ class CardProductListAdapter(
         holder.binding.subtitleTxt.text= item.deskripsi_product.toString().take(50)
             .replaceFirstChar { it.uppercase() } + "..."
         
-        // Menampilkan sisa stok
-        holder.binding.stokTxt.text = "Stok: ${item.stok_product}"
+        // Logika Validasi Stok
+        if (item.stok_product <= 0) {
+            holder.binding.stokTxt.text = "Stok Habis"
+            holder.binding.stokTxt.setTextColor(Color.RED)
+            holder.itemView.alpha = 0.5f // Membuat tampilan pudar
+        } else {
+            holder.binding.stokTxt.text = "Stok: ${item.stok_product}"
+            holder.binding.stokTxt.setTextColor(ContextCompat.getColor(context, R.color.cream_bold))
+            holder.itemView.alpha = 1.0f
+        }
 
         Glide.with(context).load(item.imgUrl).into(holder.binding.pic)
 
         holder.binding.view.setOnClickListener {
-            onAddToCart(item.documentId)
+            if (item.stok_product > 0) {
+                onAddToCart(item.documentId)
+            } else {
+                Toast.makeText(context, "Maaf, stok ${item.nama_product} telah habis!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
